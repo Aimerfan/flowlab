a565e49c1e8a8c5e0e8e74e8114da5e7d15d7c00
shan33 <sh43204327@gmail.com>
2021-08-11 08:02:22 +0800

    feat: 增加首頁圖示與登入頁面

--- core/views.py
+++ core/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+
+
+def development(request):
+    context = {}
+    return render(request, 'development/development.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'development/login.html', context)
