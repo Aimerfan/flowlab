0fb93ce33189e8be99986c997c4479ce907a9ef4
aimerfan <jvuchein@gmail.com>
2021-08-18 03:17:16 +0800

    feat: 新增 accounts.models.Student/Teacher

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class RoleBase(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='使用者')
+    full_name = models.CharField('姓名', max_length=50, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Student(RoleBase):
+    class Meta:
+        verbose_name = verbose_name_plural = '學生'
+
+
+class Teacher(RoleBase):
+    class Meta:
+        verbose_name = verbose_name_plural = '教師'
