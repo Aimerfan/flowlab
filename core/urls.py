aecd80adb1c4724236ed48a3f7c96630cbec77af
aimerfan <jvuchein@gmail.com>
2021-08-17 18:07:40 +0800

    feat: 增加 repo 頁面

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('development/', views.development, name='development'),
+    path('repository/', views.repository, name='repository'),
+    path('analysis/', views.analysis, name='analysis'),
 ]
--- core/views.py
+++ core/views.py
@@ -16,3 +16,13 @@ def index(request):
 def development(request):
     context = {}
     return render(request, 'development/development.html', context)
+
+
+def repository(request):
+    context = {}
+    return render(request, 'development/repository.html', context)
+
+
+def analysis(request):
+    context = {}
+    return render(request, 'development/analysis.html', context)
