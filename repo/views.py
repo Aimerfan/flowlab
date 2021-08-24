95008a3a0d9744e6c1a3d83017de8000468eae71
DESKTOP-98Q71FD\shan <sh43204327@gmail.com>
2021-08-25 06:21:03 +0800

    docs: 新增 vcs adapter 的註解

--- repo/views.py
+++ repo/views.py
@@ -5,7 +5,19 @@ def repo_list_view(request, user):
     """儲存庫專案列表"""
     print(request.user)
     print(user)
-    return render(request, 'repo/repo_list.html')
+    project_info = {
+        'flowlab': {
+            'name': 'flowlab',
+            'last_time': '3 minutes ago',
+            'branch_sum': '4',
+        },
+        'lab': {
+            'name': 'lab',
+            'last_time': '10 days ago',
+            'branch_sum': '2',
+        },
+    }
+    return render(request, 'repo/repo_list.html', {'projects': project_info})
 
 
 def repo_view(request, user, project):
--- vcs_adapter/base.py
+++ vcs_adapter/base.py
@@ -17,18 +17,64 @@ class VCSAdapter(metaclass=ABCMeta):
 
     @abstractmethod
     def get_repo_list(self, user):
+        """
+        取得所有 repo 名稱, 最後 commit 日期(距離今天多久前), branch 總數
+        :param user:
+        :return: {
+            'flowlab': {
+                'name': 'flowlab',
+                'last_time': '3 minutes ago',
+                'branch_sum': 4,
+            },
+        }
+        """
         pass
 
     @abstractmethod
     def get_repo(self, user, repo):
+        """
+        取得 repo 名稱, branch 總數,
+        目前 branch 名稱, 最後 commit sha, 訊息與日期(距離今天多久前)
+        :param user:
+        :param repo:
+        :return: {
+            'name': 'flowlab',
+            'branch_sum': 4,
+            'branch': 'master',
+            'sha': '78b3472',
+            'author_name': 'aimerfan',
+            'last_info': 'init commit',
+            'last_time': '3 minutes ago',
+        }
+        """
         pass
 
     @abstractmethod
     def get_branches(self, user, repo):
+        """
+        取得所有 branch 名稱
+        :param user:
+        :param repo:
+        :return: ['master', 'dev']
+        """
         pass
 
     @abstractmethod
     def get_commits(self, user, repo, branch):
+        """
+        取得該 branch 下(最新) commit 的 sha, 訊息, 作者名, 日期(距離今天多久前)
+        :param user:
+        :param repo:
+        :param branch:
+        :return: {
+            '78b3472': {
+                'sha': '78b3472',
+                'info': 'init commit',
+                'author_name': 'aimerfan',
+                'last_time': '3 minutes ago',
+            },
+        }
+        """
         pass
 
     @abstractmethod
@@ -39,6 +85,7 @@ class VCSAdapter(metaclass=ABCMeta):
         :param repo:
         :param path: 指定要搜尋的樹的路徑
         :param ref: 指定分支(或tag...), 未指定表示預設分支
+        :return: {}
         """
         pass
 
@@ -49,5 +96,6 @@ class VCSAdapter(metaclass=ABCMeta):
         :param user:
         :param repo:
         :param blob_sha: 指定要取得的檔案sha
+        :return: {}
         """
         pass
