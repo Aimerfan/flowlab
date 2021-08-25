f92505a135b32239d0372a024a357014ac40be70
DESKTOP-98Q71FD\shan <sh43204327@gmail.com>
2021-08-25 08:16:24 +0800

    feat: 新增 repo 頁面的資料傳遞
    調整項目:
    1. 使用 假資料進行 repo 頁面的資料傳遞
    2. 調整 儲存庫檔案預覽頁面之切版

--- repo/views.py
+++ repo/views.py
@@ -9,19 +9,45 @@ def repo_list_view(request, user):
         'flowlab': {
             'name': 'flowlab',
             'last_time': '3 minutes ago',
-            'branch_sum': '4',
+            'branch_sum': 4,
         },
         'lab': {
             'name': 'lab',
             'last_time': '10 days ago',
-            'branch_sum': '2',
+            'branch_sum': 1,
         },
     }
-    return render(request, 'repo/repo_list.html', {'projects': project_info})
+    return render(request, 'repo/repo_list.html', {'project_info': project_info})
 
 
 def repo_view(request, user, project):
     """儲存庫專案"""
     print(request.user)
     print(user, project)
-    return render(request, 'repo/repository.html')
+    project_info = {
+        'name': 'flowlab',
+        'branch_sum': 4,
+        'branch': 'master',
+        'sha': '78b3472',
+        'author_name': 'aimerfan',
+        'last_info': 'init commit',
+        'last_time': '3 minutes ago',
+    }
+    files = {
+        '.env': {
+            'name': '.env',
+            'last_info': 'chore',
+            'last_time': '10 days ago',
+        },
+        'main.py': {
+            'name': 'main.py',
+            'last_info': 'feat',
+            'last_time': '1 day ago',
+        },
+        'app': {
+            'name': 'app',
+            'last_info': 'feat',
+            'last_time': '4 days ago',
+        },
+    }
+    return render(request, 'repo/repository.html', {'info': project_info, 'files': files})
