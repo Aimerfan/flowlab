71e3ca04e6d1dfbeef0f62a58baffa141372d060
fcu-d0642135 <sh43204327@gmail.com>
2021-09-07 15:16:08 +0800

    feat: 新增 repo_tree page
    調整項目:
    1. 新增 repo_tree page, urls
    2. 新增 folder 與 file 的 icon 圖示
    3. 新增 repository 至 repo_tree 的超連結

--- repo/views.py
+++ repo/views.py
@@ -30,23 +30,60 @@ def repo_view(request, user, project):
         'last_time': '3 minutes ago',
     }
     files = {
+        'app': {
+            'name': 'app',
+            'format': 'folder',
+            'last_info': 'feat',
+            'last_time': '4 days ago',
+        },
         '.env': {
             'name': '.env',
+            'format': 'file',
             'last_info': 'chore',
             'last_time': '10 days ago',
         },
         'main.py': {
             'name': 'main.py',
+            'format': 'file',
             'last_info': 'feat',
             'last_time': '1 day ago',
         },
-        'app': {
-            'name': 'app',
-            'last_info': 'feat',
-            'last_time': '4 days ago',
-        },
     }
     return render(request, 'repo/repository.html', {'info': project_info, 'files': files})
+
+
+def repo_tree_view(request, user, project, file):
+    """儲存庫專案資料夾"""
+    project_info = {
+        'name': 'flowlab',
+        'branch_sum': 4,
+        'branch': 'master',
+        'sha': '78b3472',
+        'author_name': 'aimerfan',
+        'last_info': 'init commit',
+        'last_time': '3 minutes ago',
+    }
+    folder = {
+        'front': {
+            'last_info': 'init commit',
+            'last_time': '2 months ago',
+        },
+        'server': {
+            'last_info': 'init commit',
+            'last_time': '2 months ago',
+        },
+    }
+    file = {
+        'README.md': {
+            'last_info': 'init commit',
+            'last_time': '2 months ago',
+        },
+        'Jenkinsfile': {
+            'last_info': 'feat',
+            'last_time': '3 minutes ago',
+        },
+    }
+    return render(request, 'repo/repo_tree.html', {'info': project_info, 'folder': folder, 'file': file})
 
 
 def repo_blob_view(request, user, project, file):
