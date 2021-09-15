944a88bd27c72b8bc42fb7199f8c0ea7553b874d
fcu-d0642135 <sh43204327@gmail.com>
2021-09-15 08:44:05 +0800

    refactor: 移除 多餘 rollback 方法

--- features/steps/login.py
+++ features/steps/login.py
@@ -1,5 +1,4 @@
 from behave import given, when, then, step
-from django.db import transaction
 from django.contrib.auth.models import User
 
 
@@ -21,19 +20,9 @@ def enter_login_page(context, url):
 
 
 @then('The user login success')
-@transaction.atomic
 def test_login_success(context):
-    context.sid = transaction.savepoint()
     User.objects.create_user(username=context.username, password=context.password)
     response = context.test.client.login(username=context.username, password=context.password)
     context.test.assertTrue(response)
     response = context.test.client.get(context.url)
     context.test.assertContains(response, 'Logout')
-    transaction.savepoint_rollback(context.sid)
-    # transaction.set_rollback(True)
-
-
-@step('Check rollback success')
-def check_rollback_status(context):
-    response = context.test.client.login(username=context.username, password=context.password)
-    context.test.assertFalse(response)
