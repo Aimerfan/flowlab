f096c7e018059a93087b2b4c0db7e2198d66b2df
aimerfan <jvuchein@gmail.com>
2021-12-06 03:27:21 +0800

    feat: 新增 repo.model.Project

--- repo/models.py
+++ repo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from course.models import Course
+
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='使用者')
+    # 格式限制必須要跟 .form.BaseRepoForm.name 同步
+    name = models.CharField('專案名稱', max_length=50)
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    # TODO: 分組功能
+    # group = models.BooleanField
+
+    class Meta:
+        unique_together = ['user', 'name']
+        verbose_name = verbose_name_plural = '專案'
