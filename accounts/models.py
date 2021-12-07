aa4a3a422dbe82a621e5dd78f81dc48c2b071d86
aimerfan <jvuchein@gmail.com>
2021-12-08 05:32:10 +0800

    refactor: 重新命名 account model & enum

--- accounts/models.py
+++ accounts/models.py
@@ -4,13 +4,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# 每個繼承 RoleBase 的類別都要在此註冊
-class RoleEnum(Enum):
+# 每個繼承 RoleBaseModel 的類別都要在此註冊
+class Role(Enum):
     STUDENT = 1
     TEACHER = 2
 
 
-class RoleBase(models.Model):
+class RoleBaseModel(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='使用者')
     full_name = models.CharField('姓名', max_length=50, blank=True)
 
@@ -18,7 +18,7 @@ class RoleBase(models.Model):
         abstract = True
 
 
-class Student(RoleBase):
+class Student(RoleBaseModel):
     class Meta:
         verbose_name = verbose_name_plural = '學生'
 
@@ -26,7 +26,7 @@ class Student(RoleBase):
         return self.full_name
 
 
-class Teacher(RoleBase):
+class Teacher(RoleBaseModel):
     class Meta:
         verbose_name = verbose_name_plural = '教師'
 
--- accounts/utils/check_role.py
+++ accounts/utils/check_role.py
@@ -1,14 +1,15 @@
 from functools import wraps
 
 from django.shortcuts import redirect
+from django.contrib import messages
 from django.contrib.auth.models import User
 
-from ..models import RoleEnum
+from ..models import Role
 
 
 def get_roles(user: User):
     roles = []
-    for role in RoleEnum:
+    for role in Role:
         if hasattr(user, role.name.lower()):
             roles.append(role)
     return roles
@@ -21,6 +22,7 @@ def check_role(white_list):
             roles = get_roles(request.user)
             # 使用者身份與白名單有交集
             if not (set(roles) & set(white_list)):
+                messages.error(request, '你沒有權限檢視這個頁面!')
                 return redirect('index')
             else:
                 return view_func(request, *args, **kwargs)
