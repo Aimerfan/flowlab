6069f82b27a51131b985c9febc48df1e07a44dfa
D0765432 <sh43204327@gmail.com>
2021-12-08 07:55:00 +0800

    feat: 串接 course_list 頁面的資料表

--- course/views.py
+++ course/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 
 from .forms import LabForm
+from .models import Course
+from accounts.models import Role, Teacher, Student
+from accounts.utils.check_role import get_roles
 
 
 def course_list_view(request):
-    context = {}
-    return render(request, 'course_list.html', context)
+    courses = {}
+
+    if Role.STUDENT in get_roles(request.user):
+        stu_id = Student.objects.filter(full_name=request.user.username).get().id
+        courses = Course.objects.filter(students=stu_id)
+    elif Role.TEACHER in get_roles(request.user):
+        tch_id = Teacher.objects.filter(full_name=request.user.username).get().id
+        courses = Course.objects.filter(teacher=tch_id)
+
+    return render(request, 'course_list.html', {'courses': courses})
 
 
 def course_view(request, course_id):
