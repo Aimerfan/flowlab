9f0bc64bda4e1bec4038e1bf893e4c96a1c094f9
D0765432 <sh43204327@gmail.com>
2021-12-07 23:47:07 +0800

    refactor: 重構 lab 詳細頁面的表單

--- course/views.py
+++ course/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .forms import LabForm
+
 
 def course_view(request):
     context = {}
@@ -12,10 +14,10 @@ def lab_view(request, course_id):
 
 
 def lab_detail_view(request, course_id, lab_id):
-    context = {}
-    return render(request, 'lab_detail.html', context)
+    form = LabForm
+    return render(request, 'lab_detail.html', {'form': form})
 
 
 def lab_new_view(request, course_id):
-
-    return render(request, 'lab_new.html', {})
+    form = LabForm
+    return render(request, 'lab_new.html', {'form': form})
