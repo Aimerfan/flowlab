b9cc0a0f88e7ca8ae3c4c1075a402012c886f735
fcu-d0642135 <sh43204327@gmail.com>
2021-12-07 11:09:39 +0800

    feat: 新增 lab 總覽頁面

--- course/urls.py
+++ course/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import course_view
+from .views import course_view, lab_view
 
 urlpatterns = [
     path('', course_view, name='courses'),
+    path('<int:course_id>/lab', lab_view, name='labs'),
 ]
--- course/views.py
+++ course/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def course_view(request):
     context = {}
     return render(request, 'course.html', context)
+
+
+def lab_view(request, course_id):
+    context = {}
+    return render(request, 'lab.html', context)
