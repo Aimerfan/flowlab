fc9fb2192e0723aaedca1499ef87fa51e4d77def
D0765432 <sh43204327@gmail.com>
2021-12-14 21:56:19 +0800

    feat: 新增 course 相關頁面的側邊欄

--- course/views.py
+++ course/views.py
@@ -5,21 +5,24 @@ from django.http import JsonResponse
 from core.dicts import MESSAGE_DICT
 from .forms import LabForm
 from .models import Course, Lab
+from .utils import get_nav_side_dict
 from accounts.models import Role, Teacher, Student
 from accounts.utils.check_role import get_roles
 
 
 def course_list_view(request):
-    courses = {}
+    context = {}
 
     if Role.STUDENT in get_roles(request.user):
         stu_id = Student.objects.filter(full_name=request.user.username).get().id
-        courses = Course.objects.filter(students=stu_id)
+        context.update({'courses': Course.objects.filter(students=stu_id)})
+        context.update(get_nav_side_dict(request.user.username, 'student'))
     elif Role.TEACHER in get_roles(request.user):
         tch_id = Teacher.objects.filter(full_name=request.user.username).get().id
-        courses = Course.objects.filter(teacher=tch_id)
+        context.update({'courses': Course.objects.filter(teacher=tch_id)})
+        context.update(get_nav_side_dict(request.user.username, 'teacher'))
 
-    return render(request, 'course_list.html', {'courses': courses})
+    return render(request, 'course_list.html', context)
 
 
 def course_view(request, course_id):
@@ -31,8 +34,11 @@ def course_view(request, course_id):
     context.update({'students': context['course'].students.all()})
 
     if Role.STUDENT in get_roles(request.user):
+        context.update(get_nav_side_dict(request.user.username, 'student'))
         return render(request, 'course_stu.html', context)
+
     elif Role.TEACHER in get_roles(request.user):
+        context.update(get_nav_side_dict(request.user.username, 'teacher'))
         return render(request, 'course_tch.html', context)
 
 
@@ -41,10 +47,13 @@ def lab_view(request, course_id, lab_id):
     form = LabForm(instance=lab)
 
     if Role.STUDENT in get_roles(request.user):
-        return render(request, 'lab_stu.html', {
+        context = {
             'course_id': course_id,
             'lab': lab,
-        })
+        }
+        context.update(get_nav_side_dict(request.user.username, 'student'))
+        return render(request, 'lab_stu.html', context)
+
     elif Role.TEACHER in get_roles(request.user):
         # 更新 lab 資訊
         if request.method == 'POST':
@@ -58,11 +67,13 @@ def lab_view(request, course_id, lab_id):
             messages.success(request, MESSAGE_DICT.get('delete_lab_success'))
             return JsonResponse({'status': 200})
 
-        return render(request, 'lab_tch.html', {
+        context = {
             'course_id': course_id,
             'lab_id': lab_id,
             'form': form,
-        })
+        }
+        context.update(get_nav_side_dict(request.user.username, 'teacher'))
+        return render(request, 'lab_tch.html', context)
 
 
 def lab_new_view(request, course_id):
