e5528a7ddb5ed2598af2e2feea45c28b0636ceec
fcu-d0642135 <sh43204327@gmail.com>
2021-12-14 12:19:37 +0800

    refactor: 合併 models

--- flow/admin.py
+++ flow/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Project._meta.fields]
+    ordering = ['user']
