d1962aae92e2a0a721b74745320a713339b93e19
shan33 <sh43204327@gmail.com>
2021-12-15 02:37:22 +0800

    feat: 新增 course 頁面的關聯專案

--- course/views.py
+++ course/views.py
@@ -3,9 +3,11 @@ from django.contrib import messages
 from django.http import JsonResponse
 
 from core.dicts import MESSAGE_DICT
+from core.infra import GITLAB_
 from .forms import LabForm
 from .models import Course, Lab
 from .utils import get_nav_side_dict
+from flow.models import Project
 from accounts.models import Role, Teacher, Student
 from accounts.utils.check_role import get_roles
 
@@ -30,10 +32,24 @@ def course_view(request, course_id):
     context = {
         'labs': Lab.objects.filter(course=course_id),
         'course': Course.objects.filter(id=course_id).get(),
+        'project': {},
     }
     context.update({'students': context['course'].students.all()})
 
     if Role.STUDENT in get_roles(request.user):
+
+        for lab in context['labs']:
+            # 找出與 lab 關聯的專案
+            project = Project.objects.filter(user=request.user.id, labs=lab)
+            if project.count() == 1:
+                project_dict = {
+                    lab: project.get().name
+                }
+                context['project'].update(project_dict)
+            else:
+                # FIXME: 理論上不該出現這種情況 (一個 lab 對應到多個 projects)
+                pass
+
         context.update(get_nav_side_dict(request.user.username, 'student'))
         return render(request, 'course_stu.html', context)
 
