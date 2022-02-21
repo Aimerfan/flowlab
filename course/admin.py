440193afdc20c23f4c9c8a1897a5893b4aab3740
shan33 <sh43204327@gmail.com>
2022-02-22 00:10:09 +0800

    feat: 新增 問卷題目.選項.答案 model

--- course/admin.py
+++ course/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Course, Lab
+from .models import Course, Lab, Question, Option, Answer
 
 
 @admin.register(Course)
@@ -13,3 +13,21 @@ class CourseAdmin(admin.ModelAdmin):
 class LabAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Lab._meta.fields]
     filter_horizontal = ('project',)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'lab', 'type', 'content')
+    ordering = ['lab', 'number']
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'topic', 'number', 'content')
+    ordering = ['topic', 'number']
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student', 'topic', 'content')
+    ordering = ['topic']
